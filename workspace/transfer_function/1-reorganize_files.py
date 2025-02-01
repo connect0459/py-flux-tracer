6ee78f90d3e313e7d632b87627048feb4c32bd54
connect0459 <connect0459@gmail.com>
2025-02-01 19:57:55 +0900

    chore: 実行ファイルの編集

--- workspace/transfer_function/1-reorganize_files.py
+++ workspace/transfer_function/1-reorganize_files.py
@@ -2,14 +2,14 @@ import os
 from py_flux_tracer import FftFileReorganizer
 
 # 変数定義
-base_path = "/home/connect0459/labo/py-flux-tracer/workspace/transfer_function/private/2024.08.06"
-flag_file_name: str = "Flg-202406211500_202408061100.csv"
+base_path = "/mnt/c/Users/nakao/workspace/sac/transfer_function/data/ultra/2025.01.10"
+flag_filename: str = "Flg-202412231100_202501101000.csv"
 input_dir_names: list[str] = ["fft", "fft-detrend"]
 output_dir_names: list[str] = ["sorted", "sorted-detrend"]
 
 # メイン処理
 try:
-    flag_file_path: str = os.path.join(base_path, flag_file_name)
+    flag_filepath: str = os.path.join(base_path, flag_filename)
     for input_dir_name, output_dir_name in zip(input_dir_names, output_dir_names):
         input_dir_path: str = os.path.join(base_path, input_dir_name)
         output_dir_path: str = os.path.join(base_path, output_dir_name)
@@ -18,10 +18,14 @@ try:
         reorganizer = FftFileReorganizer(
             input_dir=input_dir_path,
             output_dir=output_dir_path,
-            flag_csv_path=flag_file_path,
+            flag_csv_path=flag_filepath,
             sort_by_rh=False,
         )
+        reorganizer.logger.info(
+            f"ファイルのコピーを開始します: {input_dir_name} -> {output_dir_name}"
+        )
         reorganizer.reorganize()
+        reorganizer.logger.info("ファイルのコピーが完了しました")
 except KeyboardInterrupt:
     # キーボード割り込みが発生した場合、処理を中止する
     print("KeyboardInterrupt occurred. Abort processing.")
--- workspace/transfer_function/3-plot_all_tf_curves.py
+++ workspace/transfer_function/3-plot_all_tf_curves.py
@@ -1,46 +1,54 @@
-# import matplotlib.font_manager as fm
-from py_flux_tracer import TransferFunctionCalculator
+import matplotlib.font_manager as fm
+from py_flux_tracer import FigureUtils, TransferFunctionCalculator
 
 # # フォントファイルを登録
-# font_paths: list[str] = [
-#     "/home/connect0459/.local/share/fonts/arial.ttf",  # 英語のデフォルト
-#     "/home/connect0459/.local/share/fonts/msgothic.ttc",  # 日本語のデフォルト
-# ]
-# for path in font_paths:
-#     fm.fontManager.addfont(path)
+font_paths: list[str] = [
+    "/home/connect0459/labo/py-flux-tracer/workspace/private/fonts/arial.ttf",  # 英語のデフォルト
+    "/home/connect0459/labo/py-flux-tracer/workspace/private/fonts/msgothic.ttc",  # 日本語のデフォルト
+]
+for path in font_paths:
+    fm.fontManager.addfont(path)
 
-# # フォント名を指定
-# font_array: list[str] = [
-#     "Arial",
-#     "MS Gothic",
-#     # "Dejavu Sans",
-# ]
+# フォント名を指定
+font_array: list[str] = [
+    "Arial",
+    "MS Gothic",
+]
+
+FigureUtils.setup_plot_params(font_family=font_array)
+
+# 変数定義
+tf_csv_path: str = (
+    "/mnt/c/Users/nakao/workspace/sac/transfer_function/tf-a/TF_Ultra_a.csv"
+)
+output_dir: str = (
+    "/mnt/c/Users/nakao/workspace/sac/transfer_function/outputs/all_curves"
+)
+
+# カスタムカラーの定義
+custom_colors = [
+    "#00ff00",
+    "#3cb371",
+    "#00ffff",
+    "#00bfff",
+    "#0000ff",
+    "#9400d3",
+    "#ff69b4",
+    "#000000",
+]
+
+# ガスの設定
+gas_configs = [
+    ("a_ch4-used", "CH$_4$", "red", "ch4"),
+    ("a_c2h6-used", "C$_2$H$_6$", "orange", "c2h6"),
+]
 
 # メイン処理の例
-if __name__ == "__main__":    
-    tf_csv_path: str = "/home/connect0459/labo/py-flux-tracer/workspace/senior_thesis/private/TF_Ultra_a.csv"
-    output_dir: str = "/home/connect0459/labo/py-flux-tracer/workspace/senior_thesis/private/outputs/tf"
-    
-    # カスタムカラーの定義
-    custom_colors = [
-        "#00ff00",
-        "#3cb371",
-        "#00ffff",
-        "#00bfff",
-        "#0000ff",
-        "#9400d3",
-        "#ff69b4",
-    ]
-
-    # ガスの設定
-    gas_configs = [
-        ("a_ch4-used", "CH$_4$", "red", "ch4"),
-        ("a_c2h6-used", "C$_2$H$_6$", "orange", "c2h6"),
-    ]
+if __name__ == "__main__":
     try:
         # 伝達関数曲線のプロット
         TransferFunctionCalculator.create_plot_tf_curves_from_csv(
-            file_path=tf_csv_path,
+            filepath=tf_csv_path,
             gas_configs=gas_configs,
             output_dir=output_dir,
             line_colors=custom_colors,
